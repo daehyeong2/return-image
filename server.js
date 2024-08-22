647e381a24da6f38867085807cd1d772ad590d83
daehyeong2 <baconbacon1231@gmail.com>
2024-08-22 23:35:39 +0900

    console.log 변경

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors({ origin: "*" }));
 
 app.get("/image.jpg", (req, res) => {
-  console.log("Got request.");
+  console.log(req.headers["x-forwarded-for"]);
   const imagePath = path.join(__dirname, "images", "example.jpg");
   res.setHeader("Content-Type", "image/jpeg");
   res.sendFile(imagePath);
