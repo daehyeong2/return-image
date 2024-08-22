d45abf4e3be680f5fe3621141c50d6ea12da7425
daehyeong2 <baconbacon1231@gmail.com>
2024-08-22 23:24:35 +0900

    cors 설정

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors({ origin: "*" }));
 
 app.get("/image.jpg", (req, res) => {
   console.log("Got request.");
